676cce0e2c1e46056f9b9c68c4b93dbc8bb7240f
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-10-27 20:04:09 -0300

    feat: first dashboard commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from 'next'
+
 import './globals.css'
 
+export const metadata: Metadata = {
+  title: 'Dashboard with NextJS',
+  description: 'Dashboard gerado utilizando no nextjs',
+}
+ 
 export default function RootLayout({
   children,
 }: {
