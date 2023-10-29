99099177c2549b8ef93b6a49407d1571abcb15e7
Valdir Silva <valdir.ti@gmail.com>
2023-10-29 20:46:10 -0300

    feat: add theme on header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,35 @@
+
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
+
+import useTheme from "@/app/context/useTheme"
+
 const Header = () => {
-  return (
-    <div className="flex justify-between px-4 pt-4">
-        <h2>Dashboard</h2>
-        <h2>Bem vindo, Valdir</h2>
-    </div>
-  )
+    const { theme, setTheme } = useTheme()
+
+    const handleTheme = () => {
+      if(theme === 'light') {
+          setTheme('dark')
+      } else {
+          setTheme('light')
+      }
+    }
+
+    return (
+      <div className="flex justify-between items-center px-4">
+          <h2>Dashboard</h2>
+          <div className="flex justify-around items-center">
+            <button
+                onClick={handleTheme}
+                className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block mr-6"
+                >
+                {theme === 'light' ? <BsFillMoonFill /> : <BsFillSunFill />}
+            </button>
+            <h2>
+              Bem vindo, Valdir
+            </h2>
+          </div>
+      </div>
+    )
 }
 
 export default Header
