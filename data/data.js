685282532c6f82150385500f7ca9e9ae6a4b9cb1
Valdir Silva <valdir.ti@gmail.com>
2023-10-29 22:26:28 -0300

    feat: add recent orders component

---
+++ data/data.js
@@ -0,0 +1,68 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first: 'John',
+            last: 'Doe'
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '15 minutes ago'
+    },
+    {
+        id: 2,
+        name: {
+            first: 'Chris',
+            last: 'Adams'
+        },
+        total: 1195.95,
+        status: 'Processing',
+        method: 'PayPal',
+        date: '23 minutes ago'
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Sarah',
+            last: 'Smith'
+        },
+        total: 452.75,
+        status: 'Completed',
+        method: 'Visa',
+        date: '1 Hour ago'
+    },
+    {
+        id: 4,
+        name: {
+            first: 'Joseph',
+            last: 'Choo'
+        },
+        total: 140.41,
+        status: 'Processing',
+        method: 'MasterCard',
+        date: '1 Hour ago'
+    },
+    {
+        id: 5,
+        name: {
+            first: 'Steve',
+            last: 'Harding'
+        },
+        total: 122.25,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '2 Hour ago'
+    },
+    {
+        id: 6,
+        name: {
+            first: 'Laura',
+            last: 'Croft'
+        },
+        total: 1289.33,
+        status: 'Completed',
+        method: 'PayPal',
+        date: '3 Hour ago'
+    },
+]
