349ed4683696e36cf5602b826fbb466cdcf01d05
Valdir Silva <valdir.ti@gmail.com>
2023-10-29 21:51:00 -0300

    feat: add barchart

---
+++ src/app/components/BarChart.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react'
+import { Bar } from "react-chartjs-2"
+import {
+    Chart as ChartJS, 
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from "chart.js"
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const BarChart = () => {
+
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+            datasets: [
+                {
+                    label: "Vendas R$",
+                    data: [18127, 22201, 19490, 24182, 17842, 22475, 22121],
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgb(53, 162, 235, 0.4)'
+                }
+            ],
+        })
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: 'Vendas diárias'
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+    }, [])
+
+    return (
+        <>
+            <div 
+                className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] p-4 border rounded-lg bg-white m-auto"
+            >
+                <Bar data={chartData} options={chartOptions} />
+            </div>
+        </>
+    )
+}
+
+export default BarChart
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 
 import Header from "@/app/components/Header"
 import TopCards from "@/app/components/TopCards"
+import BarChart from "@/app/components/BarChart"
+import RecentOrders from "@/app/components/RecentOrders"
 
 export default function Home() {
     return (
       <main className="bg-gray-50 min-h-screen">
         <Header />
         <TopCards />
+        <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
+          <BarChart />
+          <RecentOrders />
+        </div>
       </main>
     )
 }
